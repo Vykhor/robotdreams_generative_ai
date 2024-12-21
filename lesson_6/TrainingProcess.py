ca7fa3d3a449dc4c87c903135dce815c264f2b36
Vykhor <vykhor13@gmail.com>
2024-12-21 12:25:59 +0200

    lesson 6: add early stopping, add results

--- lesson_6/TrainingProcess.py
+++ lesson_6/TrainingProcess.py
@@ -5,7 +5,8 @@ from torch.utils.data import DataLoader
 from datasets import load_from_disk
 from transformers import T5Tokenizer, T5ForConditionalGeneration, DataCollatorWithPadding, get_scheduler
 from datetime import datetime
-from lesson_6.ModelEvaluation import evaluate_model, evaluate_metrics, generate_example
+
+from lesson_6.ModelEvaluation import evaluate_model, generate_example
 
 device = torch.device(
     "cuda" if torch.cuda.is_available()
@@ -14,30 +15,34 @@ print(f"Використовується пристрій: {device}")
 
 # Параметри
 model_name = "t5-small"
-learning_rate = 1e-5
+learning_rate = 1e-6
 weight_decay = 0.01
 batch_size = 16
-epochs = 10
+epochs = 5
 
 # Токенізатор
 tokenizer = T5Tokenizer.from_pretrained(model_name)
 tokenizer.pad_token = tokenizer.eos_token
 
 # Модель
-basic_model = T5ForConditionalGeneration.from_pretrained(model_name)
 model = T5ForConditionalGeneration.from_pretrained(model_name)
 model.to(device)
 optimizer = AdamW(model.parameters(), lr=learning_rate, weight_decay=weight_decay)
 
 # Дані
-squad_v2_dataset = load_from_disk("tokenized_squad_v2")
+dataset = load_from_disk("tokenized_dataset")
 print("Датасет tokenized_squad_v2 завантажено з диска")
 
-train_dataset = squad_v2_dataset["train"].select(range(25000))
+train_dataset = dataset["train"].select(range(int(len(dataset["train"]) * 0.3)))
 print(f"train data: {train_dataset}")
 
-validation_dataset = squad_v2_dataset["validation"]
+validation_dataset = dataset["validation"]
 print(f"validation data: {validation_dataset}")
+
+# Оцінка перед навчанням
+#basic_bleu_score, basic_rouge_score = evaluate_model(model, validation_dataset, tokenizer, device)
+#print("Оцінка моделі на валідаційному датасеті:")
+#print(f"Basic. BLEU: {basic_bleu_score['bleu']}, ROUGE-L: {basic_rouge_score['rougeL']}")
 
 data_collator = DataCollatorWithPadding(tokenizer=tokenizer, padding="longest")
 
@@ -51,6 +56,11 @@ lr_scheduler = get_scheduler(
 
 print("Cтворено даталоадери")
 
+# Парааметри для ранньої зупинки
+best_loss = float('inf')
+patience = 2
+counter = 0
+
 # Навчання
 num_batches = len(train_dataloader)
 model.train()
@@ -58,7 +68,7 @@ for epoch in range(epochs):
     total_loss = 0
     for batch_idx, batch in enumerate(train_dataloader, start=1):
         current_time = datetime.now().strftime("%H:%M:%S")
-        print(f"[{current_time}] Обробка батчу {batch_idx} з {num_batches}")
+        #print(f"[{current_time}] Обробка батчу {batch_idx} з {num_batches}")
 
         optimizer.zero_grad()
 
@@ -76,27 +86,50 @@ for epoch in range(epochs):
 
         total_loss += loss.item()
 
-        # Генерація тексту для прикладу
-        if batch_idx % 10 == 0:
-            generated_text, reference_text = generate_example(model=model,
-                                                              tokenizer=tokenizer,
-                                                              input_ids=input_ids,
-                                                              attention_mask=attention_mask,
-                                                              labels=labels)
-            print(f"Generated: {generated_text}")
-            print(f"Reference: {reference_text}")
-            bleu_score, rouge_score = evaluate_metrics([generated_text], [reference_text])
-            print(f"BLEU: {bleu_score['bleu']}, ROUGE-L: {rouge_score['rougeL']}")
-
     avg_loss = total_loss / len(train_dataloader)
+    print(f"")
     print(f"Епоха {epoch + 1} завершена, середні втрати: {avg_loss:.4f}")
-    print(f"----------------------")
+    print(f"")
 
-# Оцінка моделі
-print("Оцінка моделі на валідаційному датасеті:")
-basic_bleu_score, basic_rouge_score = evaluate_model(basic_model, validation_dataset, tokenizer, device)
-print(f"Basic. BLEU: {basic_bleu_score['bleu']}, ROUGE-L: {basic_rouge_score['rougeL']}")
+    generated_text, reference_text = generate_example(model=model,
+                                                      tokenizer=tokenizer,
+                                                      input_ids=input_ids,
+                                                      attention_mask=attention_mask,
+                                                      labels=labels)
+    print(f"Generated: {generated_text}")
+    print(f"Reference: {reference_text}")
+    print(f"")
+    avg_loss = total_loss / len(train_dataloader)
+
+    # Перевірка ранньої зупинки
+    val_loss = 0
+    model.eval()
+    with torch.no_grad():
+        for batch in validation_dataloader:
+            input_ids = batch["input_ids"].to(device)
+            attention_mask = batch["attention_mask"].to(device)
+            labels = batch["labels"].to(device)
+            outputs = model(input_ids=input_ids, attention_mask=attention_mask, labels=labels)
+            val_loss += outputs.loss.item()
+
+    val_loss /= len(validation_dataloader)
+    print(f"Валідаційні втрати: {val_loss:.4f}")
+
+    if val_loss < best_loss:
+        best_loss = val_loss
+        counter = 0
+        print("Нова найкраща модель!")
+    else:
+        counter += 1
+        print(f"Покращення не відбулося ({counter}/{patience})")
+        if counter >= patience:
+            print("Рання зупинка.")
+            break
+
+    print(f"--------------------------------------------")
+
+# Оцінка після навчанням
 bleu_score, rouge_score = evaluate_model(model, validation_dataset, tokenizer, device)
+
+print("Оцінка моделі на валідаційному датасеті:")
 print(f"Trained. BLEU: {bleu_score['bleu']}, ROUGE-L: {rouge_score['rougeL']}")
-
-
