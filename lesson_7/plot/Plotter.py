f9275b19b9e7f07a331a0ba56f8f58d69fbf556a
Vykhor <vykhor13@gmail.com>
2024-12-27 15:01:10 +0200

    lesson 7: output for generating examples

--- lesson_7/plot/Plotter.py
+++ lesson_7/plot/Plotter.py
@@ -6,8 +6,14 @@ def plot(generator, images_num, latent_dim):
     # генерація випадкових векторів
     noise = torch.randn(images_num, latent_dim)
 
+    print(f"Згенеровані вектори унікальні: {len(noise.unique(dim=0)) == images_num}")
     with torch.no_grad():
         generated_images = generator(noise).view(-1, 1, 28, 28)
+        print(f"Кількість унікальних зображень: {len(generated_images.unique(dim=0))}")
+
+    print(f"Мінімум: {generated_images.min().item()}")
+    print(f"Максимум: {generated_images.max().item()}")
+    print(f"Середнє: {generated_images.mean().item()}")
 
     # візуалізація
     grid = make_grid(generated_images, nrow=4, normalize=True)
--- lesson_7/plot/Visualization.py
+++ lesson_7/plot/Visualization.py
@@ -11,7 +11,8 @@ mnist_generator = create_generator(latent_dim, image_size)
 
 # відновлення стану генератора зі збереженого файлу
 mnist_generator.load_state_dict(torch.load("../model/mnist_generator.pth"))
-mnist_generator.eval()  # Переводимо модель в режим оцінювання
+print("Ваги першого шару:", mnist_generator[0].weight)
+mnist_generator.eval()  # переведення моделі в режим оцінювання
 print("Генератор завантажено")
 
 plot(generator=mnist_generator, images_num=16, latent_dim=latent_dim)
