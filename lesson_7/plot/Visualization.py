c3978e4d977b285a87ae66afd8e5899aff957f59
Vykhor <vykhor13@gmail.com>
2024-12-29 10:35:04 +0200

    lesson 7: add MinibatchDiscrimination layer to Discriminator, add model to results

--- lesson_7/plot/Visualization.py
+++ lesson_7/plot/Visualization.py
@@ -4,13 +4,13 @@ from lesson_7.model.Generator import create_generator
 from lesson_7.plot.Plotter import plot
 
 # створення генератора
-latent_dim = 100
+latent_dim = 256
 image_size = 28 * 28
 
 mnist_generator = create_generator(latent_dim, image_size)
 
 # відновлення стану генератора зі збереженого файлу
-mnist_generator.load_state_dict(torch.load("../model/mnist_generator.pth"))
+mnist_generator.load_state_dict(torch.load("../results/mnist_generator.pth"))
 print("Ваги першого шару:", mnist_generator[0].weight)
 mnist_generator.eval()  # переведення моделі в режим оцінювання
 print("Генератор завантажено")
