a2e04c24d167ac9988739ada0f24185a1dc05855
Vykhor <vykhor13@gmail.com>
2024-12-31 23:16:24 +0200

    lesson_7: results and summary

--- lesson_7/plot/Visualization.py
+++ lesson_7/plot/Visualization.py
@@ -9,7 +9,7 @@ image_size = 28 * 28
 mnist_generator = create_generator(latent_dim, image_size)
 
 # відновлення стану генератора зі збереженого файлу
-mnist_generator.load_state_dict(torch.load("../results/5/mnist_generator.pth"))
+mnist_generator.load_state_dict(torch.load("../results/5/mnist_generator_5.pth"))
 mnist_generator.eval()  # переведення моделі в режим оцінювання
 print("Генератор завантажено")
 
